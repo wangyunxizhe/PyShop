10ad9941c7a392221c72e3ad5610b8389871d7a3
e <543456229@qq.com>
2022-01-18 14:00:38 +0800

    将省市区数据缓存

--- apps/areas/views.py
+++ apps/areas/views.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.http import JsonResponse
 from django.shortcuts import render
 
@@ -13,15 +14,20 @@ class AreaView(View):
     '''
 
     def get(self, request):
-        # 查询省份信息
-        provinces = Area.objects.filter(parent=None)
-        province_list = []
-        # 将数据库查询到的数据对象转换成list（dict），以便于返回给前端的json格式转换
-        for province in provinces:
-            province_list.append({
-                "id": province.id,
-                "name": province.name
-            })
+        # 先尝试从缓存中读取数据
+        province_list = cache.get('province')
+        if province_list is None:
+            # 查询省份信息
+            provinces = Area.objects.filter(parent=None)
+            province_list = []
+            # 将数据库查询到的数据对象转换成list（dict），以便于返回给前端的json格式转换
+            for province in provinces:
+                province_list.append({
+                    "id": province.id,
+                    "name": province.name
+                })
+            # 将数据缓存（使用django自带工具缓存进redis）过期时长为1天
+            cache.set('province', province_list, 24 * 3600)
         return JsonResponse({'code': 0, 'errmsg': 'ok', 'province_list': province_list})
 
 
@@ -31,12 +37,17 @@ class SubAreaView(View):
     '''
 
     def get(self, request, id):
-        up_level = Area.objects.get(id=id)
-        down_level = up_level.subs.all()
-        data_list = []
-        for item in down_level:
-            data_list.append({
-                "id": item.id,
-                "name": item.name
-            })
+        # 先尝试从缓存中读取数据
+        data_list = cache.get('city:%s'%id)
+        if data_list is None:
+            up_level = Area.objects.get(id=id)
+            down_level = up_level.subs.all()
+            data_list = []
+            for item in down_level:
+                data_list.append({
+                    "id": item.id,
+                    "name": item.name
+                })
+            # 将数据缓存（使用django自带工具缓存进redis）过期时长为1天
+            cache.set('city:%s'%id, data_list, 24 * 3600)
         return JsonResponse({'code': 0, 'errmsg': 'ok', 'sub_data': {'subs': data_list}})
